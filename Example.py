c7fafbe019466ce1fec77b762fd3c7634e6b9fd5
qmor <oleg.moroz@mcc.vniiem.ru>
2016-02-04 19:57:43 +0300

    Added get_output method

--- Example.py
+++ Example.py
@@ -6,6 +6,7 @@ scpi = scpi_telnet.SCPI_Telnet("192.168.3.99",5024)
 print "power on main power"
 scpi.set_output(2,1)
 time.sleep(0.5)
+print "main output status",scpi.get_output(2)
 print "main voltage ",scpi.read_measure_volage(2)
 print "main current ",scpi.read_measure_current(2)
 
@@ -20,5 +21,8 @@ scpi.set_output(3,0)
 time.sleep(0.5)
 print "pulse voltage ",scpi.read_measure_volage(3)
 print "pulse current ",scpi.read_measure_current(3)
+print "pulse output status",scpi.get_output(3)
+
 print "main voltage ",scpi.read_measure_volage(2)
 print "main current ",scpi.read_measure_current(2)
+print "main output status",scpi.get_output(2)
--- scpi_telnet.py
+++ scpi_telnet.py
@@ -34,3 +34,18 @@ class SCPI_Telnet():
 			self.tn.write(":OUTP %s, (@" %(["off","on"][status==1]) + str(channel) + ")\n")
 			dt +=self.tn.read_some()
 			p-=1
+			
+	def get_output(self,channel):
+		self.tn.read_eager()
+		p=1
+		dt=""
+		while(p>0):
+			self.tn.write(":STAT:OPER:COND? (@" + str(channel) + ")\n")
+			dt +=self.tn.read_some()
+			p-=1
+		dt = dt.split('\n')[0].strip().replace('+','')
+		dt = int(dt)
+		if ((dt>>2)&1 == 1):#bit 3 means "1 - output off, 0 - output on"
+			return 0
+		else:
+			return 1
